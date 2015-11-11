e239054ff7f80ac04851e65454925faac766b119
Shashank Reddy <shashank.am@outlook.com>
2015-11-11 04:43:34 -0500

    run ant, ant jdepend to generate classfiles

--- routes/genClassFiles.js
+++ routes/genClassFiles.js
@@ -2,7 +2,6 @@ var xml2js = require('xml2js');
 var path = require('path');
 var fs = require('fs');
 
-
 /**
  * Check if a build.xml exists for the given project
  * @param projectPath
@@ -33,59 +32,80 @@ function search(startPath, keyFileName) {
 			search(filename, keyFileName); // recurse
 		} else if (files[i] === keyFileName) {
 			console.log('Found: ', files[i]);
-			return files[i];
+			return filename;
 		}
 	}
-	return false;
+	return null;
 }
 
-// isAntExists('/Users/shashank/Downloads/AntDateUtils');
-
-function generateClassFiles(projectPath) {
-	// check if ant exists
-	if(isAntExists(projectPath)) {
-		addJDependAntTask(antFilePath);
-		// run ant
-		var exec = require('child_process').exec;
-		// get build directory
-	}
+/**
+ * Spawns the system command. 
+ * @param cmd
+ * returns Process output
+ */
+//function runCommand(cmd) {
+//	var spawn = require('child_process').spawn;
+//	var prc = spawn(cmd);
+//	var output = "";
+//	//noinspection JSUnresolvedFunction
+//	prc.stdout.setEncoding('utf8');
+//	prc.stdout.on('data', function(data) {
+//		var str = data.toString()
+//		var lines = str.split(/(\r?\n)/g);
+//		output = lines.join("");
+//	});
+//
+//	prc.on('close', function(code) {
+//		output += 'process exit code ' + code;
+//	});
+//	return output;
+//}
+function runCommand(cmd, callback) {
+	var exec = require('child_process').exec;
+	var child = exec(cmd, function (error, stdout, stderr) {
+		  if (error !== null) {
+		    console.log('exec error: ' + error);
+		  }
+		  console.log(stdout);
+		  callback(stdout);
+		});
 }
-
-// function getBuildDirectory(antFilePath) {
-// var extractedData = "";
-// // var xml = fs.readFileSync(antFilePath);
-// var xml = "<config><test>Hello</test><test
-// myattr='myval'>AnotherHello</test><data>SomeData</data></config>";
-// var parser = new xml2js.Parser();
-// parser.parseString(xml, function(err,result){
-// //Extract the value from the data element
-// extractedData = result['config']['test'];
-// console.log(extractedData);
-// });
-// }
-
 
 function addJDependAntTask(antFilePath) {
 	var xpath = require('xpath'), dom = require('xmldom').DOMParser;
-	var xml = fs.readFileSync(antFilePath);
+	var xml = fs.readFileSync(antFilePath).toString();
 	var targetAntTask = "<target name=\"jdepend\">\n<jdepend outputfile=\"jdepend-report.txt\">\n<exclude name=\"java.*\"/>\n<exclude name=\"javax.*\"/>\n<classespath>\n<pathelement location=\"{build.dir}\" />\n</classespath>\n<classpath location=\"{build.dir}\" />\n</jdepend>\n</target>\n</project>";
 	console.log("Parsing build.xml...");
 	var doc = new dom().parseFromString(xml);
 	var destdir = xpath.select1("//target/javac/@destdir", doc).value;
-	console.log(destdir);
 	if (!destdir) {
 		throw "No destination directory for class files found in build.xml";
 	}
 	targetAntTask = targetAntTask.replace(new RegExp("{build.dir}", 'g'),
 			destdir);
 	xml = xml.replace("</project>", targetAntTask);
-	fs.writeFile(antFilePath, xml, function(err) {
-	    if(err) {
-	        return console.log(err);
-	    }
-	    console.log("Added an ant task for jDepend");
-	}); 
+	fs.writeFileSync(antFilePath, xml);
 }
-// getBuildDirectory('dfds');
 
 
+function generateClassFiles(projectPath, res) {
+	// check if ant exists
+	var antFilePath = isAntExists(projectPath);
+	console.log("Ant path: "+antFilePath);
+	if (antFilePath) {
+		addJDependAntTask(antFilePath);
+		// run ant
+		// TODO: Handle errors during ANT
+		var runAnt = runCommand('ant -f '+antFilePath, function(stdout) {
+			console.log('ant: \n' + stdout);
+			// TODO: Handle errors during build
+			var runJdepend = runCommand('ant -f '+ antFilePath +' jdepend', function(stdout){
+				console.log('\njDepend: \n' + stdout);
+			});
+		});
+	}
+}
+
+exports.generateClassFiles = function(req, res) {
+	generateClassFiles(req.query.projectPath, res);
+}
